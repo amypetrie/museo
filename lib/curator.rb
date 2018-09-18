412b42e3a699201e51f119851047513005cc81e7
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-18 10:15:50 -0600

    finishes artists with multiple photos method

--- lib/curator.rb
+++ lib/curator.rb
@@ -31,4 +31,29 @@ class Curator
     end
   end
 
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photo|
+    photo.artist_id == artist.id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    #array of artists with more than 1
+    photos_by_id= @photographs.group_by do |photo|
+      photo.artist_id
+    end
+    artist_ids = photos_by_id.map do |id, photos|
+      if photos.length > 1
+        id
+      end
+    end.compact!
+    artist_ids.map do |id|
+      find_artist_by_id(id)
+    end
+  end
+
+  def photographs_taken_by_artists_from(string)
+    #array of photos taken by a photographer from that country
+  end
+
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -111,4 +111,125 @@ class CuratorTest < Minitest::Test
     assert_instance_of Photograph, photo
     assert_equal "2", photo.id
   end
+
+  def test_find_photo_by_artist_returns_array_of_artist_photos
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+    curator = Curator.new
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+    diane_arbus = curator.find_artist_by_id("3")
+    photo_array = curator.find_photographs_by_artist(diane_arbus)
+
+    assert_instance_of Photograph, photo_array[0]
+    assert_equal "3", photo_array[0].artist_id
+  end
+
+
+  def test_artists_with_multiple_photos_returns_array_of_artists
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+    curator = Curator.new
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+    diane_arbus = curator.find_artist_by_id("3")
+
+    assert_equal [diane_arbus], curator.artists_with_multiple_photographs
+  end
 end
