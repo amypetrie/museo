538d3190df6733d7b9f519d0878c385cb9934aa4
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-18 09:37:37 -0600

    fixes curator add artist and photo methods

--- lib/curator.rb
+++ lib/curator.rb
@@ -8,8 +8,17 @@ class Curator
     @photographs = []
   end
 
-  def add_photograph(photo_object)
+  def add_photograph(photo_attributes)
+    photo_object = Photograph.new(photo_attributes)
     @photographs << photo_object
+  end
+
+  def add_artist(artist_attributes)
+    artist_object = Artist.new(artist_attributes)
+    @artists << artist_object
+  end
+
+  def find_artist_by_id(id)
   end
 
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -22,22 +22,46 @@ class CuratorTest < Minitest::Test
 
   def test_adding_photographs_adds_photos_to_photo_instance_variable
     curator = Curator.new
-    photo_1 = Photograph.new({
+    photo_1 = {
       id: "1",
       name: "Rue Mouffetard, Paris (Boy with Bottles)",
       artist_id: "1",
       year: "1954"
-    })
+    }
 
-    photo_2 = Photograph.new({
+    photo_2 = {
       id: "2",
       name: "Moonrise, Hernandez",
       artist_id: "2",
       year: "1941"
-    })
+    }
     curator.add_photograph(photo_1)
-    curator.add_photograph(photo_2
-    )
-    assert_equal [photo_1, photo_2], curator.photographs
+    curator.add_photograph(photo_2)
+
+    assert_instance_of Photograph, curator.photographs.first
+    assert_equal 2, curator.photographs.length
+  end
+
+  def test_adding_artists_adds_artists_to_artists_instance_variable
+    curator = Curator.new
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+
+    assert_instance_of Artist, curator.artists.first
+    assert_equal 2, curator.artists.length
   end
 end
